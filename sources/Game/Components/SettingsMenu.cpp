33d100284bd3afd1605ad81e66348b73568ba379
Jean-christophe BLONDEAU <jblondea@e1r10p3.42.fr>
2018-05-09 08:56:11 +0200

    window resolution ok with menu change and manual window resize

--- sources/Game/Components/SettingsMenu.cpp
+++ sources/Game/Components/SettingsMenu.cpp
@@ -120,12 +120,14 @@ void SettingsMenu::renderUI(struct nk_context *ctx)
 
 		std::stringstream ssHeight;
 		ssHeight << "Height: " << windowHeight;
+		windowHeight = BeerEngine::Window::GetInstance()->getHeight();
 		int height = windowHeight;
 		nk_label(ctx, ssHeight.str().c_str(), NK_TEXT_LEFT);
 		nk_slider_float(ctx, 0, &windowHeight, 2304.0f, 10.0f);
 
 		std::stringstream ssWidth;
 		ssWidth << "Width: " << windowWidth;
+		windowWidth = BeerEngine::Window::GetInstance()->getWidth();
 		int width = windowWidth;
 		nk_label(ctx, ssWidth.str().c_str(), NK_TEXT_LEFT);
 		nk_slider_float(ctx, 0, &windowWidth, 4096.0f, 10.0f);
